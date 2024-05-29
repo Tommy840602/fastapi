4df0ea20a6b5aa2ea025a0aa256f3a38929f1a71
Tommy840602 <tommy840602@gmail.com>
2024-05-29 23:58:29 +0800

    Add files via upload

--- blog_post.py
+++ blog_post.py
@@ -2,17 +2,25 @@ from fastapi import APIRouter,Query, Path, Body
 from pydantic import BaseModel
 from typing import Optional
 from typing import List
+from typing import Dict
 
 router = APIRouter(
     prefix="/blog",
     tags=["blogs"],
 )
 
+class Image(BaseModel):
+    url:str
+    alias:str
+
 class BlogModel(BaseModel): 
     title:str
     content:str
     published:Optional[bool]
-    comments_nums:int
+    nb_comments:int
+    tags:List[str]=[]
+    metadata: Dict[str, str] = {"key1": "value1"}
+    image: Optional[Image] = None    
 
 @router.post('/new')
 def create_blog(blog: BlogModel):
@@ -26,14 +34,25 @@ def create_blog(blog: BlogModel,id: int,version: int=1):
         "version":version
         }
 
-@router.post('/new/{id}/comment')
-def create_comment(blog: BlogModel,id: int,
-                   comment_id: int=Query(None
-                                         ,title="Comment ID的標題"
-                                         ,description="Comment ID的一些說明"
-                                         ,alias="commentId"
-                                         ,deprecated=True),
-    content: str = Body(..., min_length=12,max_length=24,regex="^[a-z\\s]*$"),
-    version: Optional[List[str]] = Query(None)):           
-    return {"blog": blog, "id": id, "comment_id": comment_id,"content":content,"version":version}
+@router.post("/new/{id}/comment/{comment_id}")
+def create_comment(
+    blog: BlogModel, id: int,
+    comment_title: int = Query(None,
+                               title="Comment Title的標題",
+                               description="Comment Title的一些說明", alias="commentTitle",
+                               deprecated=True,
+                               ),
+    content: str = Body(..., min_length=10, max_length=50, regex='^[a-z\\s]*$'),
+    version: Optional[List[str]] = Query(['1.0', '1,5', '2.0']),
+    comment_id: int = Path(..., le=5)
+    ):
+
+    return {
+            "blog": blog,
+            "id": id,
+            "comment_title": comment_title,
+            "content": content,
+            "version": version,
+            "comment_id": comment_id
+            }
 
